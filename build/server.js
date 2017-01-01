49ccb36105f129739950b0a019ffa341fed34853
Peterfurax <peterfurax@gmail.com>
2017-01-01 22:54:55 +0100

    update server.js for test

--- build/server.js
+++ build/server.js
@@ -1,66 +1,73 @@
 'use strict';
 
+/*jshint esversion: 6 */
 var path = require('path');
 var os = require('os');
 var fs = require('fs');
 var Busboy = require('busboy');
 var express = require('express');
-var bodyParser = require('body-parser');
 var app = express();
-var HTTP_PORT = 8000;
-
-//parse application/json and look for raw text
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: 'application/json' }));
-// app.get("/", (req, res) => res.json({message: "Welcome to our Bookstore!"}));
+app.port = 8e3;
+// Express Routeur
 app.route("/").get(function (req, res) {
-  return getReceiver(req, res);
+  return app.getReceiver(req, res);
 }).post(function (req, res) {
-  return postReceiver(req, res);
+  return app.postReceiver(req, res);
 });
-
-function getReceiver(req, res) {
+/**
+ * [response from GET http json]
+ * @method getReceiver
+ * @param  {[array]}    req [request]
+ * @param  {[array]}    res [response]
+ * @return {[objet]}        [json]
+ */
+app.getReceiver = function (req, res) {
   res.json({
     message: "you will get nothing here"
   });
   res.end();
-}
-
-function postReceiver(req, res) {
-  // busboy init capture req.headers
+};
+/**
+ * [response from POST Request]
+ * @method postReceiver
+ * @param  {[array]}    req [request]
+ * @param  {[array]}    res [response]
+ * @return {[type]}         [busboy]
+ */
+app.postReceiver = function (req, res) {
+  // -- Busboy -- init capture req.headers
   var busboy = new Busboy({
     headers: req.headers
   });
-  // console.log(req.headers);
-  // busboy init on file datastream type
+  // -- Busboy -- on -- file
   busboy.on('file', function (fieldname, file, filename, encoding, mimetype) {
     console.log('Filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
-    // busboy on progress
-    file.on('data', function (data) {
-      // console.log('Filename [' + filename + '] got ' + data.length + ' bytes');
-    });
-    // busboy end transfert, error
+    // -- Busboy -- on -- data
+    // file.on('data', data => {
+    // console.log('Filename [' + filename + '] got ' + data.length + ' bytes');
+    // });
+    // -- Busboy -- end --
     file.on('end', function () {
       console.log('Filename [' + filename + '] Finished');
     });
-    // saveFolder dir, filemane
+    // SaveFolder dir, filemane
     var saveTo = path.join(os.tmpdir(), path.basename(filename));
-    // file piping => writeStreamFile
+    // File piping => writeStreamFile
     file.pipe(fs.createWriteStream(saveTo));
   });
-  // busboy finish
+  // -- Busboy -- finish
   busboy.on('finish', function () {
-    // writeHead
+    // Return writeHead
     res.writeHead(200, {
       'Connection': 'close'
     });
-    // return res by .end('string')
+    // Return end
     res.end('Upload is Finished!');
   });
-  // busboy pipe request
+  // Req.pipe(busboy)
   return req.pipe(busboy);
-}
-app.listen(HTTP_PORT);
+};
+// Express up app.port
+app.listen(app.port);
+// for debug support
 module.exports = app;
