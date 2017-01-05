b322e96a818e104ebe77c09e736199146aa4cc22
Peterfurax <peterfurax@gmail.com>
2017-01-05 03:58:08 +0100

    put http

--- src/server.js
+++ src/server.js
@@ -1,5 +1,4 @@
-/*jslint node: true */
-/*jshint esversion: 6 */
+"use strict";
 const path = require('path');
 const os = require('os');
 const fs = require('fs');
@@ -8,18 +7,21 @@ const express = require('express');
 const app = express();
 app.port = 8e3;
 // Express Routeur
-app.route("/").get((req, res) => app.getReceiver(req, res)).post((req, res) => app.postReceiver(req, res));
+app.route("/").get((req, res) => app.getReceiver(req, res)).post((req, res) => app.postReceiver(req, res)).put((req, res) => app.postReceiver(req, res));
 /**
- * [response from GET http json]
+ * [GET response from http req return a json message]
  * @method getReceiver
  * @param  {[array]}    req [request]
  * @param  {[array]}    res [response]
- * @return {[objet]}        [json]
+ * @return {[objet]}        [message: "you will get nothing here"]
  */
 app.getReceiver = (req, res) => {
+  console.log(req);
+  // response Json res
   res.json({
     message: "you will get nothing here"
   });
+  // response ending
   res.end();
 };
 /**
@@ -34,12 +36,13 @@ app.postReceiver = (req, res) => {
   const busboy = new Busboy({
     headers: req.headers
   });
+  console.log(req.headers);
   // -- Busboy -- on -- file
   busboy.on('file', (fieldname, file, filename, encoding, mimetype) => {
     console.log('Filename: ' + filename + ', encoding: ' + encoding + ', mimetype: ' + mimetype);
-    // -- Busboy -- on -- data
+    // -- Busboy -- on -- progress data
     // file.on('data', data => {
-      // console.log('Filename [' + filename + '] got ' + data.length + ' bytes');
+    //   console.log('Filename [' + filename + '] got ' + data.length + ' bytes');
     // });
     // -- Busboy -- end --
     file.on('end', () => {
@@ -52,17 +55,17 @@ app.postReceiver = (req, res) => {
   });
   // -- Busboy -- finish
   busboy.on('finish', () => {
-    // Return writeHead
+    // response writeHead
     res.writeHead(200, {
       'Connection': 'close'
     });
-    // Return end
+    // response end
     res.end('Upload is Finished!');
   });
-  // Req.pipe(busboy)
+  // init a .pipe on the req to busboy()
   return req.pipe(busboy);
 };
-// Express up app.port
+// Express up htpp on app.port
 app.listen(app.port);
 // for debug support
 module.exports = app;
