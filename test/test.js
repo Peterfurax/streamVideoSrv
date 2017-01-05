3254457188756232517de1e9b98fd386c77b297d
Peterfurax <peterfurax@gmail.com>
2017-01-05 03:57:39 +0100

    test put

--- test/test.js
+++ test/test.js
@@ -1,3 +1,5 @@
+"use strict";
+/*jslint node: true */
 /*jshint esversion: 6 */
 // Require the dev-dependencies
 let fs = require('fs');
@@ -5,7 +7,6 @@ let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../dist/server.min.js');
 let should = chai.should();
-
 // During the test the env variable is set to test
 process.env.NODE_ENV = 'test';
 // Chai use
@@ -14,7 +15,6 @@ describe('app', () => {
   describe('#port', () => {
     it('should be a number', (done) => {
       server.port.should.be.a('number');
-      (10).should.not.equal(0);
       done();
     });
     it('should be equal to 8000', (done) => {
@@ -24,31 +24,37 @@ describe('app', () => {
   });
 });
 describe('Api response ', () => {
-  describe('#POST()', () => {
-    it('should accept image multipart/form-data to : / ', (done) => {
-      chai.request(server).post('/')
-      .set('content-type', 'multipart/form-data')
-      .attach('imageField', fs.readFileSync('test/img.jpg'), 'img.jpg')
-      .end((err, res) => {
+  describe('#GET', () => {
+    it('should accept GET from : / ', (done) => {
+      chai.request(server).get('/').end((err, res) => {
         res.should.have.status(200);
         done();
-        });
-    });
-    it('should accept video multipart/form-data to : / ', (done) => {
-      chai.request(server).post('/')
-      .set('content-type', 'multipart/form-data')
-      .attach('imageField', fs.readFileSync('test/video.mp4'), 'video.mp4')
-      .end((err, res) => {
-        res.should.have.status(200);
-        done();
-        });
+      });
     });
   });
-  describe('#GET', () => {
-    it('should accept /GET to : / ', (done) =>{
-      chai.request(server)
-      .get('/')
-      .end((err, res) => {
+  describe('#POST', () => {
+    it('should accept POST image multipart/form-data from : / ', (done) => {
+      chai.request(server).post('/').set('content-type', 'multipart/form-data').attach('imageField', fs.readFileSync('test/img.jpg'), 'img.jpg').end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+    });
+    it('should accept POST video multipart/form-data from : / ', (done) => {
+      chai.request(server).post('/').set('content-type', 'multipart/form-data').attach('imageField', fs.readFileSync('test/video.mp4'), 'video.mp4').end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+    });
+  });
+  describe('#PUT', () => {
+    it('should accept PUT image multipart/form-data from : / ', (done) => {
+      chai.request(server).put('/').set('content-type', 'multipart/form-data').attach('imageField', fs.readFileSync('test/img.jpg'), 'img.jpg').end((err, res) => {
+        res.should.have.status(200);
+        done();
+      });
+    });
+    it('should accept PUT video multipart/form-data from : / ', (done) => {
+      chai.request(server).put('/').set('content-type', 'multipart/form-data').attach('imageField', fs.readFileSync('test/video.mp4'), 'video.mp4').end((err, res) => {
         res.should.have.status(200);
         done();
       });
